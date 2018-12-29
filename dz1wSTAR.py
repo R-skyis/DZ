37c15e4e106e0d588b36e596c0d5e94eaed597ff
R-skyis <radkovskyis@gmail.com>
2018-12-29 17:06:46 +0200

    new file

---
+++ dz1wSTAR.py
@@ -0,0 +1,7 @@
+a = 7
+b = 10
+print("a =", a, "b =", b)
+tmp = a
+a = b
+b = tmp
+print("a =", a, "b =", b)
---
+++ dz1wSTARv3.py
@@ -0,0 +1,5 @@
+a = 7
+b = 10
+print("a =", a, "b =", b)
+a, b = b, a
+print("a =", a, "b =", b)
