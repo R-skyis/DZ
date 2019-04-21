58a2daababa2a743b8fe8feb55226dac5aac43d5
Sergey Radkovskyi <46136676+R-skyis@users.noreply.github.com>
2019-04-21 12:27:52 +0300

    Update dz3.py

--- 03_functions/dz3.py
+++ 03_functions/dz3.py
@@ -1,4 +1,3 @@
-c = 0
 def is_prime(a):
     for i in range(2, a // 2 + 1):
         if a % i == 0:
@@ -9,12 +8,10 @@ def is_prime(a):
 
 a = is_prime(1)
 
-num1 = int(input("От:"))
-num2 = int(input("До:"))
 
 def count(n):
     c = 0
-    for num in range(num1, num2):
+    for num in range(2, n * n):
         if is_prime(num):
             c += 1
             if c > n:
